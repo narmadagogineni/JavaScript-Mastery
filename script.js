cff9ae369bf6c32ca2cf05479f27664f4109eed6
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-08-28 22:28:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,68 @@
-// let hindi = 80;
-// let eng = 90;
-// let mat = 100;
-// let avg = (hindi + eng + mat)/3;
+//AVG
 
-
+let hindi = 80;
+let eng = 90;
+let mat = 100;
+let avg = (hindi + eng + mat)/3;
+-------------------------------------------
+  
 //concatenation
-// firstName = "Narmada";
-// lastName = "Gogineni";
-// fullName = firstName + " " + lastName;
-// console.log(fullName);
 
-
-// name = "Narmada Gogineni";
-// typeof name;
-// console.log(name.length);
-// name[0];
-
-
+firstName = "Narmada";
+lastName = "Gogineni";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+---------------------------------------------
+//String
+  
+name = "Narmada Gogineni";
+typeof name;
+console.log(name.length);
+name[0];
+---------------------------------------------
 //TEMPLATE LITERALS
-// let a = 5;
-// let b = 2;
-// console.log(`Amount to be paid is ${a+b} rupees`);
 
+let a = 5;
+let b = 2;
+console.log(`Amount to be paid is ${a+b} rupees`);
 
-// let eraserPrice = 5;
-// let pencilPrice = 10;
-// let output = (`The total price to be paid is ${eraserPrice + pencilPrice} rupees`);
-// console.log(output);
+//EX 2
 
-//ARITHEMATIC OPERATIONS
-// let a = 10;
-// let b = 5;
-// console.log(a+b);
-// console.log(a-b);
-// console.log(a*b);
-// console.log(a/b);
-// console.log(a%b);
-// console.log(a**b);
+let eraserPrice = 5;
+let pencilPrice = 10;
+let output = (`The total price to be paid is ${eraserPrice + pencilPrice} rupees`);
+console.log(output);
+----------------------------------------------------------------------------------------
 
-//UNARY OPERATOR
-// a = 5;
-// console.log(a++);
-// console.log(++a);
-// console.log(a--);
-// console.log(--a);
+ // ARITHEMATIC OPERATIONS
+  
+let a = 10;
+let b = 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+----------------------------
+  
+  //UNARY OPERATOR
+  
+a = 5;
+console.log(a++);
+console.log(++a);
+console.log(a--);
+console.log(--a);
+--------------------------------
 
 //comparison operators
+  
 let age = 18;
 console.log(age>18);
+--------------------------------
+  
+
+
+
+
+  
