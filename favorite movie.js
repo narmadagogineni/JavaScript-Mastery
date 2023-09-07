41651f8c527bfeca43eed9e938309de508faeb24
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-09-07 12:43:00 +0530

    Update favorite movie.js

--- favorite movie.js
+++ favorite movie.js
@@ -1,1 +1,14 @@
 // Guess my fav movie
+
+const favMovie = "avatar";
+let guess = prompt("Guess my favorite movie: ");
+
+while((guess != favMovie) && (guess != "quit")) {
+    guess = prompt("Wrong guess! Please try again..");
+}
+
+if(guess == "avatar") {
+    console.log("Congrats! You guessed it right");
+} else {
+    console.log("You Quit!");
+}
