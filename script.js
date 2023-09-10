3597ac90137f007d6f1530290e9fc87064b50ed7
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-09-10 21:07:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1250,10 +1250,33 @@ console.log(num);
 //ex4 TO GENERATE FROM 21 TO 25
 let num = Math.floor(Math.random() * 5 ) + 21;
 console.log(num);
------------------------------------------------
+-------------------------------------------------------------------------
 
+//TASK 3
+  
+const max = prompt("Enter the max range of number you want to play: ");
 
+const rand = Math.floor(Math.random() * max) + 1;
+// console.log(rand);
+let guess = prompt("Guess the number: ")
+while(true) {
+    if(guess == "quit") {
+        alert("Quitting the game");
+        break;
+    }
 
+    if(guess == rand) {
+        alert('You got it right! Congrats!!', rand);
+        console.log('You got it right! Congrats!!, The random number was', rand);
+        break;
+    } else if(guess < rand) {
+        guess = prompt("Hint: Enter a larger number!");
+    } else if(guess > rand) {
+        guess = prompt("Hint: Enter a smaller number!");
+    }
+
+}
+--------------------------------------------------------------------------------
 
 
 
