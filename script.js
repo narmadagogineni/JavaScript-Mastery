386a6918a86c215f453d631c52482df12ec89d46
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-09-10 11:41:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -905,7 +905,7 @@ while(i>=1) {
     i= i-2;
 }
 ----------------------------------------------
-//fav movie task
+//FAV =ORITE MOVIE task 2
 
   const favMovie = "avatar";
 let guess = prompt("Guess my Favorite movie: ");
@@ -1020,8 +1020,40 @@ for(let list of lists) {
     }
 }
 ================================================================
+//TODO APP task 2
+  
+let todo = [];
 
+let req = prompt("Enter your REQUEST:");
 
+while(true) {
+    if(req == "quit") {
+        console.log("quitting the App");
+        break;
+    }
+
+    if(req == "list") {
+        console.log("----------------------");
+        for(let i = 0; i<todo.length; i++) {
+            console.log(i, todo[i]);
+        }
+        console.log("----------------------");
+    } else if(req == "add") {
+        let task = prompt("Enter a task to ADD");
+        todo.push(task);
+        console.log("1 new task added");
+    } else if(req == "delete") {
+        let idx = prompt("Enter the index od task to be deleted");
+        todo.splice(idx, 1);
+        console.log("Task deleted");
+     } else { 
+             console.log("Wrong request")
+             }
+
+    req = prompt("Enter your REQUEST:");
+
+}
+======================================================================
 
 
 
